df004b8ce4948d8e2d69399f22066ff30b2e701e
Shri-gif <271024975+Shri-gif@users.noreply.github.com>
2026-04-02 13:39:47 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 // Supabase config
 const SUPABASE_URL = "https://gpcbkguyrkluazkznybf.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_G68X4gRScisCoGJTEKirFA_bXi-NWcL";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwY2JrZ3V5cmtsdWF6a3pueWJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNzMwMTIsImV4cCI6MjA5MDY0OTAxMn0.NqG6ggDw2xV2mHv1B0HB78c6Td-xMgOCtGTNnpgMatw";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY); 
 
