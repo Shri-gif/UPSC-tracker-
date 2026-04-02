a4118001c6f070ad74c005fad24854f6ddb49c5c
Shri-gif <271024975+Shri-gif@users.noreply.github.com>
2026-04-02 10:26:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+
+// 🔑 Supabase Config (YAHAN APNI KEY DAAL)
+const SUPABASE_URL = "https://gpcbkguyrkluazkznybf.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwY2JrZ3V5cmtsdWF6a3pueWJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNzMwMTIsImV4cCI6MjA5MDY0OTAxMn0.NqG6ggDw2xV2mHv1B0HB78c6Td-xMgOCtGTNnpgMatw;
+ 
+// Init
+const { createClient } = supabase;
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 supabaseClient.auth.onAuthStateChange((event, session) => {
   if (session) {
     currentUser = session.user;
@@ -5,15 +14,6 @@ supabaseClient.auth.onAuthStateChange((event, session) => {
     loadData();
   }
 });
-
-
-// 🔑 Supabase Config (YAHAN APNI KEY DAAL)
-const SUPABASE_URL = "https://gpcbkguyrkluazkznybf.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwY2JrZ3V5cmtsdWF6a3pueWJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNzMwMTIsImV4cCI6MjA5MDY0OTAxMn0.NqG6ggDw2xV2mHv1B0HB78c6Td-xMgOCtGTNnpgMatw;
-
-// Init
-const { createClient } = supabase;
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // DOM
 const loginBtn = document.getElementById("loginBtn");
