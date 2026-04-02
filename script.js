e0304562f91baa7b67326f7d15b0eb74f072e065
Shri-gif <271024975+Shri-gif@users.noreply.github.com>
2026-04-02 10:16:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+supabaseClient.auth.onAuthStateChange((event, session) => {
+  if (session) {
+    currentUser = session.user;
+    showDashboard();
+    loadData();
+  }
+});
+
+
 // 🔑 Supabase Config (YAHAN APNI KEY DAAL)
 const SUPABASE_URL = "https://gpcbkguyrkluazkznybf.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwY2JrZ3V5cmtsdWF6a3pueWJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNzMwMTIsImV4cCI6MjA5MDY0OTAxMn0.NqG6ggDw2xV2mHv1B0HB78c6Td-xMgOCtGTNnpgMatw;
@@ -32,7 +41,7 @@ async function checkUser() {
   }
 }
 
-checkUser();
+setTimeout(checkUser, 1000);
 
 // login/signup
 authForm.addEventListener("submit", async (e) => {
