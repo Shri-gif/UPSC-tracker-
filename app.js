b0f7429939a14d50251871cd92a7967f2ed14ecb
Shri-gif <271024975+Shri-gif@users.noreply.github.com>
2026-04-06 09:59:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -100,6 +100,12 @@ borderWidth: 2
 options: {
 responsive: true,
 maintainAspectRatio: false
+  scales: {
+    y: {
+      beginAtZero: true,
+      min: 0   // 🔥 IMPORTANT LINE
+    }
+  }
 }
 });
 }
