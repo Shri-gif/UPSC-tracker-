2ee096eaae86d035d25ebe7901f9c193b0568e80
Shri-gif <271024975+Shri-gif@users.noreply.github.com>
2026-05-03 10:03:30 +0530

    Delete components directory

--- components/WelcomeCard.jsx
+++
@@ -1,9 +0,0 @@
-const WelcomeCard = ({ onFirstClick }) => (
-  <div className="welcome-card">
-    <h2>Welcome to Daily Analysis</h2>
-    <p>Click any topic to start reading</p>
-    <button onClick={onFirstClick}>🚀 Start Reading</button>
-  </div>
-);
-
-export default WelcomeCard;
